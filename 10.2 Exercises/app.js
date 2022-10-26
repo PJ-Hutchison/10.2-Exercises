c8aa076008ffb09dc1fb35ca6d0f67da652d0552
PJ-Hutchison <100161458+PJ-Hutchison@users.noreply.github.com>
2022-10-26 15:34:42 -0500

    Add files via upload

---
+++ 10.2 Exercises/app.js
@@ -0,0 +1,14 @@
+const Pi = 3.14;
+//This stops Pi from being redeclared or reassigned.
+
+// What is the difference between var and let?
+// Var can be redeclared and reassigned, let only lets you reassign. Let creates a block scope.
+
+// What is the difference between var and const?
+// Var can be redeclared and reassigned, but const can't be redeclared or reassigned. Const also creates a block scope.
+
+// What is the difference between let and const?
+// You can use let to reassign, but const can't be reassigned.
+
+// What is hoisting?
+// Hoisting is when a compiler "moves" variables to the top, so they are declared before the rest of the code runs.
